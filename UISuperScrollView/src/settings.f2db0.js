6f980a8306f8a96c762f08bc8d420de1d6861557
steveJobs <icipiqkm@gmail.com>
2020-11-14 03:21:20 +0400

    1

---
+++ UISuperScrollView/src/settings.f2db0.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/HORIZONTAL.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "a8060",
+        main: "26cb0"
+    }
+};
